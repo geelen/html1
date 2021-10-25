1f8865aa9b29190a54728f6445ccacf551df57a4
Cina Saffary <itscina@gmail.com>
2021-10-25 14:40:17 -0500

    fix id

--- functions/todos/index.ts
+++ functions/todos/index.ts
@@ -5,11 +5,10 @@ export async function onRequest() {
 }
 
 export async function onRequestGet() {
-  const todos = Array.from({ length: 5 }, () => ({ id }))
+  const todos = Array.from({ length: 5 }, () => ({ id: id() }))
   return new Response(JSON.stringify(todos), { status: 200 })
 }
 
 export async function onRequestPost() {
-  const id = Math.floor(Math.random() * 1000) + 1
-  return new Response(JSON.stringify({ id }))
+  return new Response(JSON.stringify({ id: id() }))
 }
